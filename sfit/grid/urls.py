9aed12f360cc3b97570381d4e71ecdf4e82be491
Griffith Rees <griff.rees@gmail.com>
2010-06-11 13:25:46 -0600

    Fix design template.

--- sfit/grid/urls.py
+++ sfit/grid/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from grid.models import Design
+
+design_dict = {
+        'design' : Design.objects.latest(),
+        }
 
 urlpatterns = patterns('',
-        (r'^api/', include('sfit.grid.api.urls'))
+        (r'^api/', include('sfit.grid.api.urls')),
+        (r'^', direct_to_template, {'template': 'base.html', 'extra_context': design_dict}),
         )
