712af4fb8b841b58f2b3598e994bf8ef023e86a2
Griffith Rees <griff.rees@gmail.com>
2010-06-11 00:08:43 -0600

    Add models schema, with Designs and Deltas.

--- sfit/grid/models.py
+++ sfit/grid/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Delta(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    edges_h   = models.CharField(max_length=4160)
+    edges_v   = models.CharField(max_length=4160)
+    diag_se   = models.CharField(max_length=4096)
+    diag_sw   = models.CharField(max_length=4096)
+    cell      = models.PositiveSmallIntegerField()
+    user      = models.ForeignKey(User, related_name='deltas')
+
+    def __unicode__(self):
+        return '%s: %s' % (self.user, self.timestamp)
+
+class Design(models.Model):
+    slug      = models.SlugField()
+    name      = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    deltas    = models.ForeignKey('Delta', related_name='design')
+
+    def __unicode__(self):
+        return self.name
