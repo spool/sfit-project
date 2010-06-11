9db137d6f3e3d7fc221c7caabaf1db4b6c8e3916
Griffith Rees <griff.rees@gmail.com>
2010-06-10 18:03:51 -0600

    Install grid in settings.py

--- sfit/settings.py
+++ sfit/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'grid',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
 )
