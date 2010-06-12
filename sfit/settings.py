66f268b47adbee06a4e841f90c29f03497ecf210
Griffith Rees <griff.rees@gmail.com>
2010-06-12 11:25:11 -0600

    Add admindocs to settings and urls

--- sfit/settings.py
+++ sfit/settings.py
@@ -95,8 +95,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
     'piston',
     'grid',
     'south',
-    'django.contrib.admin',
 )
