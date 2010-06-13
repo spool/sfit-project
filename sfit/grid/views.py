6e95861d220daaa322a1dc1f009042655f32e690
Griffith Rees <griff.rees@gmail.com>
2010-06-12 21:45:19 -0600

    Include latest delta in view.

--- sfit/grid/views.py
+++ sfit/grid/views.py
@@ -2,10 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.views.generic.simple import direct_to_template
 from django.shortcuts import get_object_or_404, render_to_response
 from grid.models import *
+from django.contrib.auth.models import User
 
 @login_required
 def grid_edit(request, slug=None):
-    if slug: d = get_object_or_404(Design, slug=slug)
-    else: d = Design.objects.last()
-    return render_to_response('base.html', {'design': d})
+    if slug: design = get_object_or_404(Design, slug=slug)
+    else: design = Design.objects.last()
+    delta = request.user.deltas.last()
+    return render_to_response('base.html', {'design': design, 'delta': delta})
 
