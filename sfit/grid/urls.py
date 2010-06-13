a02766f3e10ad9058b108369973e6dcbd4f3ba2a
Griffith Rees <griff.rees@gmail.com>
2010-06-12 23:34:46 -0600

    Fix extra forward slash and login required on urls

--- sfit/grid/urls.py
+++ sfit/grid/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 from grid.models import Design
+from grid.views import grid_edit
 
 design_dict = {
         'design' : Design.objects.last(),
@@ -8,5 +9,6 @@ design_dict = {
 
 urlpatterns = patterns('',
         (r'^api/', include('sfit.grid.api.urls')),
-        (r'^/', direct_to_template, {'template': 'base.html', 'extra_context': design_dict}),
+        #(r'^', direct_to_template, {'template': 'base.html', 'extra_context': design_dict}),
+        (r'^', grid_edit),
         )
