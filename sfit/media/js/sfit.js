483cacc5f16ff417274d461246d16ae0ff8909d0
root <root@muon.nitric.net>
2010-06-14 09:27:56 +0100

    commit

--- sfit/media/js/sfit.js
+++ sfit/media/js/sfit.js
@@ -8,13 +8,26 @@ function load_data(data)
 	graph[2] = (data.diag_se);
 	graph[3] = (data.diag_sw);
 
-	$.get("/media/js/sfit-draw.pjs?" + (Math.random() * 5000), function(code) {
-		canvas = $("#canvas")[0];
-		processing = Processing(canvas, code);
-                //calling from JS to PJS:
-		processing.setData(graph);
-		processing.setCell(data.cell);
-	}); 
+	$.get("/media/js/sfit-draw.pjs?" + (Math.random() * 5000),
+		function(code) {
+			alert("loading, cell: " + data.cell);
+			canvas = $("#canvas")[0];
+			processing = Processing(canvas, code);
+                	//calling from JS to PJS:
+			processing.setData(graph);
+			if (data.cell > -1)
+				processing.setCell(data.cell);
+		});
+}
+
+function load_empty()
+{
+	$.get("/media/js/sfit-draw.pjs?" + (Math.random() * 5000),
+		function(code) {
+			canvas = $("#canvas")[0];
+			processing = Processing(canvas, code);
+		}
+	);
 }
 
 function saved(output)
@@ -38,11 +51,13 @@ function save()
 		success: saved
 	});
 	*/
+	cell = processing.getCell();
 	input = document.getElementById("edges_h"); input.value = data[0];
 	input = document.getElementById("edges_v"); input.value = data[1];
 	input = document.getElementById("diag_se"); input.value = data[2];
 	input = document.getElementById("diag_sw"); input.value = data[3];
-	input = document.getElementById("cell"); input.value = 0;
+	input = document.getElementById("cell"); input.value = cell;
+	alert("Saving with cell " + cell);
 	form = document.getElementById("form"); form.submit();
 }
 
@@ -52,7 +67,8 @@ $(document).ready(function()
 	// url = "/media/js/data.html";
 	$.ajax({
 		url: url,
-		success: load_data
+		success: load_data,
+		error: load_empty
 	});
 	$('#save').bind('click', save);
 });
